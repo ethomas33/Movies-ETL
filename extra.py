cf8267200d2ae073ae872a5125c12b25d9d5232e
Elizabeth Thomas <elizabeththomas33@gmail.com>
2022-03-18 18:57:29 -0400

    8.5.1

---
+++ extra.py
@@ -0,0 +1,21 @@
+directory = [
+    {'name': "Megan Bailey", 'hair': "Black", 'eyes': "Brown", 'species': 'dog', 'nationality': "USA"},
+    {'name': "Wallace", 'hair': None, 'eyes': "black", 'species': 'human', 'nationality': "UK"},
+    {'name': "Gromit", 'hair': "brown", 'eyes': "black", 'species': 'dog', 'nationality': "UK"},
+    {'name': "Wendoline Ramsbottom", 'hair': "brown", 'eyes': "Black", 'species': 'human', 'nationality': "UK"},
+    {'name': "Mr. Blobby", 'hair': "pink", 'eyes': "green", 'species': 'blob', 'nationality': "UK"},
+    {'name': "Bob McKenzie", 'hair': "brown", 'eyes': "green", 'species': 'human', 'nationality': "CA"},
+    {'name': "Doug McKenzie", 'hair': "blond", 'eyes': "blue", 'species': 'human', 'nationality': "CA"},
+    ]
+# Store some people in variables [] are index operator 
+megan = directory[0]
+wendoline = directory[3]
+blobby = directory[4]
+# Fill in the following strings:
+print(f"Megan had {megan['eyes']} eyes and {megan['hair']} hair.")
+print(f"Wendoline has {wendoline['eyes']} eyes and {wendoline['hair']} hair.")
+print(f"Mr. Blobby has {blobby['eyes']} eyes and {blobby['hair']} hair.")
+# make a function to return True or False if the person is British
+def is_british(person):
+    # your code here
+    pass
